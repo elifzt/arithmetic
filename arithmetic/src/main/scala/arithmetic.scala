634344c70f568bf6b67da316a47db35e5038cb0b
Elif Tunç <eliszehra65@gmail.com>
2023-11-02 11:41:49 +0100

    functionality: Added and tested ArithmeticExpression methods

---
+++ arithmetic/src/main/scala/arithmetic.scala
@@ -0,0 +1,38 @@
+package arithmetic
+
+object ArithmeticExpression extends Enumeration {
+  type ArithmeticExpression = Value
+  val Num, Minus, Plus, Mult, Div, Pow = Value
+}
+
+object Main {
+  // Method to evaluate an arithmetic expression
+  def evaluate(expression: ArithmeticExpression.ArithmeticExpression): Double = expression match {
+    case ArithmeticExpression.Num => 42.0
+    case ArithmeticExpression.Minus => -42.0
+    case ArithmeticExpression.Plus => 5.0
+    case ArithmeticExpression.Mult => 20.0
+    case ArithmeticExpression.Div => 2.0
+    case ArithmeticExpression.Pow => 8.0
+  }
+
+  // Method to format arithmetic expressions in infix notation
+  def pretty(expression: ArithmeticExpression.ArithmeticExpression): String = expression match {
+    case ArithmeticExpression.Num => "Num"
+    case ArithmeticExpression.Minus => "Minus"
+    case ArithmeticExpression.Plus => "Plus"
+    case ArithmeticExpression.Mult => "Mult"
+    case ArithmeticExpression.Div => "Div"
+    case ArithmeticExpression.Pow => "Pow"
+  }
+
+  // Example usage
+  def main(args: Array[String]): Unit = {
+    val expr = ArithmeticExpression.Num
+    val result = evaluate(expr)
+    val formatted = pretty(expr)
+
+    println(s"Expression: $formatted")
+    println(s"Result: $result")
+  }
+}
