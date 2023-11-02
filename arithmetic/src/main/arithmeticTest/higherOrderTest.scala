674bc15622cf12d7abaf2ee7bb157c2c1c35436d
Elif Tunç <eliszehra65@gmail.com>
2023-11-02 12:18:34 +0100

    lists:

--- arithmetic/src/main/arithmeticTest/higherOrderTest.scala
+++ arithmetic/src/main/arithmeticTest/higherOrderTest.scala
@@ -1,3 +1,32 @@
-class higherOrderTest {
-  
+class ArithmeticTest extends AnyFunSuite {
+  test("evaluate should return a list of correct values for multiple expressions") {
+    val expressions = List(
+      ArithmeticExpression.Num,
+      ArithmeticExpression.Plus,
+      ArithmeticExpression.Minus,
+      // Diğer ifadeleri burada ekleyin
+    )
+
+val expectedResults = List(42.0, 7.0, -35.0)
+
+    val results = ArithmeticExpression.evaluate(expressions)
+    assert(results == expectedResults)
+  }
+
+  test("showResults should return the expressions in a readable format") {
+    val expressions = List(
+      ArithmeticExpression.Num,
+      ArithmeticExpression.Plus,
+      ArithmeticExpression.Minus,
+      // Diğer ifadeleri burada ekleyin
+    )
+
+    val expectedOutput =
+      """Num = 42.0
+        |Plus = 7.0
+        |Minus = -35.0
+        |""".stripMargin
+    val output = ArithmeticExpression.showResults(expressions)
+    assert(output == expectedOutput)
+  }
 }
