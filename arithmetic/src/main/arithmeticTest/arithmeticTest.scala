3bd635599a47161f548ce0dc5980fea10d6249bb
Elif Tunç <eliszehra65@gmail.com>
2023-11-02 11:55:59 +0100

    all updated

--- arithmetic/src/main/arithmeticTest/arithmeticTest.scala
+++ arithmetic/src/main/arithmeticTest/arithmeticTest.scala
@@ -4,11 +4,19 @@ import arithmetic.ArithmeticExpression
 class ArithmeticTest extends AnyFunSuite {
   test("evaluate should return the correct value") {
     assert(ArithmeticExpression.evaluate(ArithmeticExpression.Num) == 42.0)
-    // Diğer evaluate testleri için assert ifadelerini burada ekleyin
   }
 
   test("pretty should return the correct string representation") {
     assert(ArithmeticExpression.pretty(ArithmeticExpression.Num) == "Num")
-    // Diğer pretty testleri için assert ifadelerini burada ekleyin
   }
+
+  test("evaluate should handle Pow") {
+  assert(ArithmeticExpression.evaluate(ArithmeticExpression.Pow, 2, 4) == 16)
+}
+
+test("pretty should handle Pow") {
+  assert(ArithmeticExpression.pretty(ArithmeticExpression.Pow) == "Pow")
+}
+
+
 }
